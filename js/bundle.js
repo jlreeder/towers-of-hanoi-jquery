2ffbefeeb140adae134e6e10ace16f94f1db3192
Jason Reeder <info@jasonreeder.com>
2017-02-27 17:59:27 -0800

    Configure view

--- js/bundle.js
+++ js/bundle.js
@@ -156,6 +156,7 @@ class View{
 
     for (let i = 0; i < 3; i++) {
       let $pile = $("<ul></ul>");
+      $pile.addClass("pile");
       if (i === 0) {
         for (let j = 0; j < 3; j++) {
           let $li = $("<li></li>");
